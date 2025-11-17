562cdb07ce4f7079c6d683a6b60a758b6eb85217
absherif19 <abdelrahman.gamal@psinv.net>
2025-11-17 15:39:44 +0400

    fixing review comments

--- features/landing/components/OurServices/card.tsx
+++ features/landing/components/OurServices/card.tsx
@@ -5,13 +5,15 @@ import { ArrowRight } from "lucide-react";
 
 const Card = ({ title, description, image }: ServiceItem) => {
   return (
-    <div className="min-w-[460px] rounded-2xl shadow-[0px_2px_4px_0px_rgba(0,_0,_0,_0.1)]">
+    <div className="min-w-[320px] md:min-w-[460px] rounded-2xl shadow-[0px_2px_4px_0px_rgba(0,_0,_0,_0.1)]">
+      {" "}
       <Image
         src={image}
         alt={title}
         className="rounded-t-2xl"
         width={460}
         height={192}
+        loading="lazy"
       />
       <div className="bg-white rounded-b-2xl px-6 py-4">
         <h1 className="text-[20px] mb-2.5 font-bold">{title}</h1>
--- features/landing/components/OurServices/our-services.tsx
+++ features/landing/components/OurServices/our-services.tsx
@@ -4,7 +4,8 @@ import Heading from "./heading";
 
 const OurServices = () => {
   return (
-    <div className="py-10 px-20 gap-12 w-full flex flex-col items-center ">
+    <div className="py-10 px-4 sm:px-8 md:px-12 lg:px-20 gap-12 w-full flex flex-col items-center">
+      {" "}
       <Heading />
       <CardsGrid />
       <Button className="px-5 py-6 bg-[#2D343E] text-lg font-semibold">
