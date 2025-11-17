308998b42cdcfbdba6f4e6b27bfdf3f42274b747
Mohamed sayed <mhmdsayed90031@gmail.com>
2025-11-17 10:45:50 +0200

    solce images sizes

--- features/products/components/filter-by-industry.tsx
+++ features/products/components/filter-by-industry.tsx
@@ -5,7 +5,9 @@ const FilterByIndustry = () => {
 
     const RENDER_SECTORS = sectors.map((s) => (
         <div key={s.id} className="space-y-2.5 flex flex-col items-center">
-            <Image src={s.src} alt={s.title} width={76} height={76} />
+            <div className="min-w-[76px] min-h-[76px] relative">
+                <Image src={s.src} alt={s.title} fill className="object-cover" />
+            </div>
             <span className="block text-sm font-normal leading-5">
                 {s.title}
             </span>
