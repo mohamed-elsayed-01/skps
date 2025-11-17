dafbfc187dced3f7fa74e87af2b8c9e1a4f34bf0
absherif19 <abdelrahman.gamal@psinv.net>
2025-11-17 11:11:55 +0400

    add why choose us section

---
+++ features/landing/components/WhyChooseUS/main-why-choose-us.tsx
@@ -0,0 +1,13 @@
+import Features from "./Features";
+import Heading from "./heading";
+
+const MainWhyChooseUs = () => {
+  return (
+    <section className="flex flex-col items-center justify-center py-20 px-[120px]">
+      <Heading />
+      <Features />
+    </section>
+  );
+};
+
+export default MainWhyChooseUs;
---
+++ features/landing/hooks/useInterval.ts
@@ -0,0 +1,20 @@
+"use client";
+
+import { useEffect, useRef } from "react";
+
+export function useInterval(callback: () => void, delay: number) {
+  const savedCallback = useRef(callback);
+
+  // Save the latest callback
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  // Set up the interval
+  useEffect(() => {
+    if (delay === null) return;
+
+    const id = setInterval(() => savedCallback.current(), delay);
+    return () => clearInterval(id);
+  }, [delay]);
+}
