8277a41fae7f3420e0cfba0f72f8d8f8e3b5fa87
absherif19 <abdelrahman.gamal@psinv.net>
2025-11-18 15:01:56 +0400

    adding our clients section

---
+++ features/landing/components/OurClients/our-clients.tsx
@@ -0,0 +1,13 @@
+import Heading from "./heading";
+import LogosGrid from "./logos-grid";
+
+const OurClients = () => {
+  return (
+    <div className="flex flex-col items-center bg-white py-16">
+      <Heading />
+      <LogosGrid />
+    </div>
+  );
+};
+
+export default OurClients;
